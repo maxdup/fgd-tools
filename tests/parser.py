c9f62f1f3e09e21277053789bc99a6efb4fcf5a1
maxdup <mdupuis@hotmail.ca>
2019-08-18 16:27:19 -0400

    setup unittest, tox and coverage

---
+++ tests/parser.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+
+from fgdtools import FgdParse, FgdWrite
+
+
+class ParseTestCase(unittest.TestCase):
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def test_parse(self):
+        self.assertTrue(FgdParse)
